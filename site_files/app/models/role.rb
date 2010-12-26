38ec3a662a4a847fe9f3e678271d51584830518e
Pedro Pereira <pedro.mdp@gmail.com>
2010-12-26 15:25:50 +0000

    Cleaned deprecated files for diagram generation

--- site_files/app/models/role.rb
+++ site_files/app/models/role.rb
@@ -3,8 +3,7 @@ class Role < ActiveRecord::Base
   # Relationships
   # ==========================================================================
   
-  has_and_belongs_to_many :groups
-  has_many                :users    , :through => 'groups'
+  has_many                :users
 
 
   # ==========================================================================
