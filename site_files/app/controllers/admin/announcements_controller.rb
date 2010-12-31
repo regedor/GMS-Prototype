0a34261fff2d726d229529c15d936df11ccfbc07
Nuno Azevedo <nfsazevedo@gmail.com>
2010-12-31 04:13:58 +0000

    Added support for announcements' backgrounds. Images can be imported at the backend and shown in the announcements at the frontend root. Only works for one chosen format so far (currently png) and images are not upscaled.

--- site_files/app/controllers/admin/announcements_controller.rb
+++ site_files/app/controllers/admin/announcements_controller.rb
@@ -1,13 +1,34 @@
 class Admin::AnnouncementsController < Admin::BaseController
 
-  active_scaffold :announcement do |config|
+  before_filter :change_background_form_ui, :only => [:new, :edit]
+  after_filter :save_background, :only => [:create, :update]
 
+  active_scaffold :announcement do |config|
     Scaffoldapp::active_scaffold config, "admin.announcements",
       :list         => [ :title, :headline, :starts_at, :ends_at ],
       :show         => [ :title, :starts_at, :ends_at, :background, :headline, :message ],
       :create       => [ :title, :starts_at, :ends_at, :background, :headline, :message ],
       :edit         => [  ]
-
   end
+
+  private
+
+    def change_background_form_ui
+      active_scaffold_config.create.columns.each do |column|
+        column.form_ui = :file if column.name == :background
+      end
+      active_scaffold_config.create.multipart = true
+      active_scaffold_config.update.multipart = true
+    end
+
+    def save_background
+      if upload = params[:record][:background]
+        directory = "public/images/announcements"
+        Dir.mkdir directory unless File.exists? directory
+        path = File.join("public/images/announcements", "#{@record.id}.png")
+        File.open(path, "wb") { |file| file.write upload.read }
+        @record.update_attribute :background, path
+      end
+    end
 
 end
