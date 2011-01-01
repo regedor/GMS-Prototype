3acc72f6472263fa3e69c30fb7156d5098d2a123
Zamith <zamith.28@gmail.com>
2011-01-01 21:57:43 +0000

    Yaml Editor fully operational and commented

--- site_files/app/models/yaml_editor.rb
+++ site_files/app/models/yaml_editor.rb
@@ -22,17 +22,30 @@ class YamlEditor
     @options_hash = {}
   end
 
+
+  # Loads a YAML file
   def load(filename,options_hash={})
     @filename = filename
     @file_hash = YAML::load_file(filename)
     @options_hash = options_hash
   end
 
+  # Saves a hash as YAML in  the file in filename
   def save
     file_str = YAML::dump @file_hash
     File.open(@filename, 'w') {|f| f.write(file_str)}
   end
 
+  # Given a path (eg: "development.theme") and a newValue, sets the value in that path from the file_hash
+  def set_value_from_path(path_to_value,newValue)
+    path_array = path_to_value.split "."
+    last_key = path_array.pop
+    hash = create_hash_with_path(path_array,{last_key=>newValue})
+    self.merge!(@file_hash,hash)
+  end  
+
+  # Given a path (eg: "development.theme"), gets the value in that path from the file_hash and returns the HTML code corresponding 
+  # to what was asked for that value, in the options_hash
   def get_value_from_path(path_to_value)
     path_array = path_to_value.split "."
     current_value = @file_hash
@@ -41,16 +54,22 @@ class YamlEditor
     end 
     
     case @options_hash[path_to_value]['type']
-      when "text_field" then return "<input type=\"text\" value=#{current_value} /></br>"
+      when "text_field" then return "<input name=#{path_to_value} type=\"text\" value=#{current_value} /></br>"
       else raise ArgumentError, "Unhandled type #{@options_hash[path_to_value]['type']}"  
     end    
   end  
 
+  # Merges two nested hashes, keeping the values in merge2, inc ase of conflict
   def merge!(merge1,merge2)
-    merge1.merge!(merge2) {|key,old_value,new_value| self.merge!(old_value,new_value)} if merge1 && merge2
+    case merge1
+      when String then merge2
+      else merge1.merge!(merge2) {|key,old_value,new_value| self.merge!(old_value,new_value)} if merge1 && merge2
+    end    
   end  
 
-  def get_only_some_attributes(attr_hash,file_hash=@file_hash) # {"en"=>["navigation"=>[{"title"}]]}
+  # Filters nested hashes. Receives the hash of attributes to filter (eg: {"en"=>[{"navigation"=>[{"title"}]}]})
+  # returns the file_hash but only with the requested attributes values 
+  def get_only_some_attributes(attr_hash,file_hash=@file_hash)
     attr_hash.each_pair do |k,v|
       @path << k
       case v
@@ -64,6 +83,9 @@ class YamlEditor
     return @final_hash
   end
   
+  private 
+  
+  # Used when an array is found in the nested hash
   def get_array_values(array,file_hash)
     array.each do |value|
       case value
@@ -75,6 +97,8 @@ class YamlEditor
     
   end  
   
+  # Receives array (eg: ["dev","example"]) and hash (eg: {"theme"=>"simon"}), 
+  # and returns a hash (eg: {"dev"=>{"example"=>{"theme"=>simon}}})
   def create_hash_with_path(path_array,hash)
     newHash = Hash.new
     tempHash = Hash.new
