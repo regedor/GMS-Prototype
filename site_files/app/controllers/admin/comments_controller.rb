d58d250d485076c3c43efd28a958b25eca02221b
Nuno <duke@duke-laptop.(none)>
2011-01-06 20:45:35 +0000

    Fixed minor bug in comments controller (deleted field no longer exists)

--- site_files/app/controllers/admin/comments_controller.rb
+++ site_files/app/controllers/admin/comments_controller.rb
@@ -29,8 +29,8 @@ class Admin::CommentsController < Admin::BaseController
 
   # Override this method to define conditions to be used when querying a recordset (e.g. for List).
   # With this, only the comments with the value 'false' in the column 'deleted' will be shown.
-  def conditions_for_collection
-    return { :deleted => false }
-  end
+  #def conditions_for_collection
+  #  return { :deleted => false }
+  #end
 
 end
