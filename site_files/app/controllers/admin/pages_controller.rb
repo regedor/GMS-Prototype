4b3b97a8679274b530c60ff3ca7e7122a745dd8e
Zamith <zamith.28@gmail.com>
2011-01-17 02:46:20 +0000

    Fully functional pages with priority

--- site_files/app/controllers/admin/pages_controller.rb
+++ site_files/app/controllers/admin/pages_controller.rb
@@ -2,7 +2,7 @@ class Admin::PagesController < Admin::BaseController
   active_scaffold :page do |config|
     Scaffoldapp::active_scaffold config, "admin.pages",
       :list   => [ :title, :excert, :created_at, :total_approved_comments ],
-      :create => [ :title, :body, :slug, :show_in_navigation, :has_comments ],
+      :create => [ :title, :body, :slug, :show_in_navigation, :has_comments, :priority ],
       :edit   => [  ]
   end
 
