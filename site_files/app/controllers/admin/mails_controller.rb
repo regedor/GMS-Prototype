3bd45d903b5298ad99c2c4ba1757c4805ab1a455
Zamith <zamith.28@gmail.com>
2011-02-16 01:32:24 +0000

    Mail stuff

--- site_files/app/controllers/admin/mails_controller.rb
+++ site_files/app/controllers/admin/mails_controller.rb
@@ -29,7 +29,7 @@ class Admin::MailsController < Admin::BaseController
   end   
  
   def create
-    recipients_array = params[:mail][:recipients_text].split(",")
+    recipients_array = params[:mail][:recipients_text].split(", ")
     users_to_send = []
     @result = []
     users_and_groups = [] 
@@ -53,7 +53,10 @@ class Admin::MailsController < Admin::BaseController
     mail.subject = params[:mail][:subject]
     mail.message = params[:mail][:message]
     mail.user_id = current_user.id
-    @result = (Mail.send_emails(users_to_send,mail)) ? users_to_send.map(&:email) : "fail"
+    (Mail.send_emails(users_to_send,mail)) ? flash[:notice] = t("admin.mails.send_ok") : flash[:error] = t("admin.mails.send_fail")
+    
+    @mail = Mail.new 
+    render :new
   end  
   
 end  
