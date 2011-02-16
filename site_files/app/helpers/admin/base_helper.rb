15b5de5d6d86f36355e261ec498f7f1a0bf0d256
Nuno Azevedo <nfsazevedo@gmail.com>
2011-02-16 03:21:35 +0000

    Changed some datetime picker stuff.

--- site_files/app/helpers/admin/base_helper.rb
+++ site_files/app/helpers/admin/base_helper.rb
@@ -69,18 +69,18 @@ module Admin::BaseHelper
   end
 
   def starts_at_column(record)
-    if (Time.now - record.starts_at) < 30.days
-      I18n::t "generic_sentence.time_ago", :time_ago => time_ago_in_words(record.starts_at)
+    if record.starts_at == record.starts_at.at_beginning_of_day
+      record.starts_at.strftime('%d/%m/%Y')
     else
-      record.starts_at.strftime('%d %b, %Y')
+      record.starts_at.strftime('%d/%m/%Y %H:%M')
     end
   end
 
   def ends_at_column(record)
-    if (record.ends_at - Time.now) < 30.days
-      I18n::t "generic_sentence.time_after", :time_after => time_ago_in_words(record.ends_at)
+    if record.ends_at == record.ends_at.at_beginning_of_day
+      record.ends_at.strftime('%d/%m/%Y')
     else
-      record.ends_at.strftime('%d %b, %Y')
+      record.ends_at.strftime('%d/%m/%Y %H:%M')
     end
   end
   
