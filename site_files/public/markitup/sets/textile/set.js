27365d2f219e432471bcc02977d000e5c4bfd47a
Miguel Regedor <miguelregedor@gmail.com>
2011-04-07 15:08:17 +0100

    Textile editor is beeing improving

--- site_files/public/markitup/sets/textile/set.js
+++ site_files/public/markitup/sets/textile/set.js
@@ -1,40 +1,119 @@
 // -------------------------------------------------------------------
-// markItUp!
+// Copyright (C) 2011 Group Buddies
 // -------------------------------------------------------------------
-// Copyright (C) 2008 Jay Salvat
+// Based on Jay Salvat work
 // http://markitup.jaysalvat.com/
 // -------------------------------------------------------------------
-// Textile tags example
+// Textile tags 
 // http://en.wikipedia.org/wiki/Textile_(markup_language)
 // http://www.textism.com/
 // -------------------------------------------------------------------
-// Feel free to add more tags
-// -------------------------------------------------------------------
 mySettings = {
-	previewParserPath:	'', // path to your Textile parser
-	onShiftEnter:		{keepDefault:false, replaceWith:'\n\n'},
-	markupSet: [
-		{name:'Heading 1', key:'1', openWith:'h1(!(([![Class]!]))!). ', placeHolder:'Your title here...' },
-		{name:'Heading 2', key:'2', openWith:'h2(!(([![Class]!]))!). ', placeHolder:'Your title here...' },
-		{name:'Heading 3', key:'3', openWith:'h3(!(([![Class]!]))!). ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'h4(!(([![Class]!]))!). ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'h5(!(([![Class]!]))!). ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'h6(!(([![Class]!]))!). ', placeHolder:'Your title here...' },
-		{name:'Paragraph', key:'P', openWith:'p(!(([![Class]!]))!). '},
-		{separator:'---------------' },
-		{name:'Bold', key:'B', closeWith:'*', openWith:'*'},
-		{name:'Italic', key:'I', closeWith:'_', openWith:'_'},
-		{name:'Stroke through', key:'S', closeWith:'-', openWith:'-'},
-		{separator:'---------------' },
-		{name:'Bulleted list', openWith:'(!(* |!|*)!)'},
-		{name:'Numeric list', openWith:'(!(# |!|#)!)'}, 
-		{separator:'---------------' },
-		{name:'Picture', replaceWith:'![![Source:!:http://]!]([![Alternative text]!])!'}, 
-		{name:'Link', openWith:'"', closeWith:'([![Title]!])":[![Link:!:http://]!]', placeHolder:'Your text to link here...' },
-		{separator:'---------------' },
-		{name:'Quotes', openWith:'bq(!(([![Class]!]))!). '},
-		{name:'Code', openWith:'@', closeWith:'@'},
-		{separator:'---------------' },
-		{name:'Preview', call:'preview', className:'preview'}
-	]
+  onShiftEnter:		{keepDefault:false, replaceWith:'\n\n'},
+  markupSet: [
+    {name:'Heading 1', className:'heading1',
+      key:'1', openWith:'h1(!(([![Class]!]))!). ', placeHolder:'Your title here...'
+    },
+    {name:'Heading 2', className:'heading2',
+      key:'2', openWith:'h2(!(([![Class]!]))!). ', placeHolder:'Your title here...'
+    },
+    {name:'Heading 3', className:'heading3',
+      key:'3', openWith:'h3(!(([![Class]!]))!). ', placeHolder:'Your title here...'
+    },
+    {name:'Heading 4', className:'heading4',
+      key:'4', openWith:'h4(!(([![Class]!]))!). ', placeHolder:'Your title here...'
+    },
+    {name:'Paragraph', className:'paragraph',
+      key:'P', openWith:'p(!(([![Class]!]))!). '
+    },
+    {separator:'---------------' },
+    {name:'Bold', className:'bold',
+      key:'B', closeWith:'*', openWith:'*'
+    },
+    {name:'Italic', className:'italic',
+      key:'I', closeWith:'_', openWith:'_'
+    },
+    {name:'Underline', className:'underline',
+      key:'U', openWith:'[u]', closeWith:'[/u]'
+    },
+    {name:'Stroke through', className:'stroke',
+      key:'S', closeWith:'-', openWith:'-'
+    },
+    {name:'Colors', className:'colors',
+      openWith:'[color=[![Color]!]]', closeWith:'[/color]', dropMenu: [
+        {name:'Yellow', openWith:'[color=yellow]', closeWith:'[/color]', className:"col1-1" },
+        {name:'Orange', openWith:'[color=orange]', closeWith:'[/color]', className:"col1-2" },
+        {name:'Red',    openWith:'[color=red]',    closeWith:'[/color]', className:"col1-3" },
+        {name:'Blue',   openWith:'[color=blue]',   closeWith:'[/color]', className:"col2-1" },
+        {name:'Purple', openWith:'[color=purple]', closeWith:'[/color]', className:"col2-2" },
+        {name:'Green',  openWith:'[color=green]',  closeWith:'[/color]', className:"col2-3" },
+        {name:'White',  openWith:'[color=white]',  closeWith:'[/color]', className:"col3-1" },
+        {name:'Gray',   openWith:'[color=gray]',   closeWith:'[/color]', className:"col3-2" },
+        {name:'Black',   openWith:'[color=black]', closeWith:'[/color]', className:"col3-3" }
+      ]
+    },
+    {separator:'---------------' },
+    {name:'Bulleted list', className:'bulleted',
+      openWith:'(!(* |!|*)!)'
+    },
+    {name:'Numeric list', className:'numeric',
+      openWith:'(!(# |!|#)!)'
+    }, 
+    {separator:'---------------' },
+    {name:'Link', className:'link',
+      openWith:'"', closeWith:'([![Title]!])":[![Link:!:http://]!]', placeHolder:'Your text to link here...' 
+    },
+    {name:'Quotes', className:'quotes', 
+      openWith:'bq(!(([![Class]!]))!). '
+    },
+    {name:'Table generator', className:'tablegenerator', 
+      placeholder:"Your text here...",
+      replaceWith:function(h) {
+      	cols = prompt("How many cols?");
+      	rows = prompt("How many rows?");
+      	html = "";
+      	for (r = 0; r < rows; r++) {
+      	  for (c = 0; c < cols; c++) {
+      	  	html += "|"+(h.placeholder||"");	
+      	  }
+      	  html += "|\n";
+      	}
+      	return html;
+      }
+    },
+    {separator:'---------------' },
+    {name:'Calculator', className:'calculator',
+         className:'calculator',
+         replaceWith:function(markItUp) { 
+            try { 
+                return eval(markItUp.selection); 
+            }
+            catch(e){} 
+         }
+        }, 
+    {name:'Sort', className:"sort",
+         replaceWith:function(markItUp) { 
+            var s = markItUp.selection.split((($.browser.mozilla) ? "\n" : "\r\n"));
+            s.sort();
+            if (markItUp.altKey) s.reverse();
+            return s.join("\n");
+         }
+        },    
+    {name:'Date', className:"date", 
+         replaceWith:function() { 
+            var date = new Date()
+            var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            var monthname = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+            var D = weekday[date.getDay()];
+            var d = date.getDate();
+            var m = monthname[date.getMonth()];
+            var y = date.getFullYear();
+            var h = date.getHours();
+            var i = date.getMinutes();
+            var s = date.getSeconds();
+            return (D +" "+ d + " " + m + " " + y + " " + h + ":" + i + ":" + s);
+         }
+        }
+
+  ]
 }
