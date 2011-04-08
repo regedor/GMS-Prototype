8b93953bf03ee17242dfc10db3c477de20d41c1a
Miguel Regedor <miguelregedor@gmail.com>
2011-04-08 02:36:56 +0100

    themes system started

--- site_files/config/compass.rb
+++ site_files/config/compass.rb
@@ -4,8 +4,8 @@ project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/stylesheets"
-sass_dir = "app/stylesheets"
+css_dir = "public/stylesheets/"
+sass_dir = "app/themes/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
---
+++ site_files/lib/tasks/theme.rake
@@ -0,0 +1,13 @@
+namespace :themes do
+  task :choose => :environment do
+    puts "Select the theme that you want to activate:"
+    paths = Dir[RAILS_ROOT+'/app/themes/overrides/*']
+    paths.each_with_index do | path, i | 
+      puts i.to_s + " - " + path.split("/").last 
+    end
+    theme_path = paths[STDIN.gets.chomp.to_i]
+    cp_r(theme_path+'/.', '/Users/regedor/coiso') 
+
+    #puts "----"+theme+"--"
+  end
+end
