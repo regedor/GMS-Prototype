cf0df4bbee36256d9fe2f4677273b7746c08c50b
Pedro Pereira <pedro.mdp@gmail.com>
2011-04-08 18:34:20 +0100

    Textile related changes: it's looking good for the time being!

--- site_files/config/compass.rb
+++ site_files/config/compass.rb
@@ -5,7 +5,7 @@ project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/"
-sass_dir = "app/themes/stylesheets"
+sass_dir = "app/themes/sass"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
