68492196f51afb35ad9b7c7dc7d9aaf628b6768e
Zamith <zamith.28@gmail.com>
2011-05-28 17:32:25 +0100

    Corrected events per day

--- site_files/app/helpers/calendar_helper.rb
+++ site_files/app/helpers/calendar_helper.rb
@@ -11,19 +11,7 @@ module CalendarHelper
       :event_strips => event_strips,
       :month_name_text => I18n.localize(shown_month, :format => "%B %Y"),
       :previous_month_text => "<< " + month_link(shown_month.prev_month),
-      :next_month_text => month_link(shown_month.next_month) + " >>",
-      :width => nil,
-      :heigth => 400,
-      
-      :day_names_height => 18,
-      :day_nums_height => 18,
-      :event_height => 15,
-      :event_margin => 1,
-      :event_padding_top => 1,
-      
-      :use_all_day => true,
-      :use_javascript => true,
-      :link_to_day_action => false
+      :next_month_text => month_link(shown_month.next_month) + " >>"
     }
   end
 
