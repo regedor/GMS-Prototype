e19af261587271e6d6c3e85ec17380aa4fb9b7b2
Zamith <zamith.28@gmail.com>
2011-06-25 15:15:17 +0100

    Minor changes

--- site_files/app/controllers/admin/pages_controller.rb
+++ site_files/app/controllers/admin/pages_controller.rb
@@ -1,5 +1,6 @@
 class Admin::PagesController < Admin::BaseController
   filter_access_to :all, :require => any_as_privilege
+  
 
   active_scaffold :page do |config|
     Scaffoldapp::active_scaffold config, "admin.pages",
@@ -27,5 +28,16 @@ class Admin::PagesController < Admin::BaseController
       format.js   { render :partial => 'preview' }
     end
   end
+  
+  def update
+    @page = Page.find(params[:id])
+    @page.update_attributes params[:record]
+    if @page.save
+      flash[:notice] = t("flash.page_updated", :name => @page.title)
+      redirect_to admin_pages_path
+    else
+      flash[:error] = t("flash.page_not_updated", :name => @page.title)
+    end      
+  end  
 
 end
