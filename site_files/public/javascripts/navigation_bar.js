90f02a3c9e5fe6ed3443b8c546f3321075cb03bc
Zamith <zamith.28@gmail.com>
2011-08-23 15:27:15 +0100

    Added js to add current page class and to scroll to content Created partial for the head

---
+++ site_files/public/javascripts/navigation_bar.js
@@ -0,0 +1,13 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+			
+	if(window.location.pathname !== "/")
+	{
+		var new_postion = $('#content').offset();
+		$('html, body').animate({scrollTop:new_postion.top-20}, 1000);
+    }
+			
+	$(".current").removeClass("current");
+	$('a[href='+unescapeFromUrl(window.location.pathname,"UTF-8")+'#content]').addClass("current");
+});
