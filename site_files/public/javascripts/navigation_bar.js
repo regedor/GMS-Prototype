906db59188585008dbd134c5e1eae287a470c917
Zamith <zamith.28@gmail.com>
2011-09-01 23:00:43 +0100

    Removing scroll

--- site_files/public/javascripts/navigation_bar.js
+++ site_files/public/javascripts/navigation_bar.js
@@ -1,13 +1,6 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function($) {
-			
-	if(window.location.pathname !== "/")
-	{
-		var new_postion = $('#content').offset();
-		$('html, body').animate({scrollTop:new_postion.top-20}, 1000);
-    }
-			
 	$(".current").removeClass("current");
 	$('a[href='+unescapeFromUrl(window.location.pathname,"UTF-8")+']').addClass("current");
 });
