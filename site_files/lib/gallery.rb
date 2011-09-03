b39849643828a9a2aaf0577913d8ee5477e5255f
Zamith <zamith.28@gmail.com>
2011-09-03 02:17:11 +0100

    First step towards modularity

--- site_files/lib/gallery.rb
+++ site_files/lib/gallery.rb
@@ -5,8 +5,8 @@ class Gallery
       Dir.new(basedir).entries.drop 2  # Remove . and ..
     end
     
-    def image_size(image_name,basedir="public/assets/images/thumbs")
-      %x[$MAGICK_HOME/utilities/identify -format %w,%h #{basedir}/#{image_name}].chomp!
+    def image_size(image_name,basedir="public/assets/images")
+      %x[identify -format %w,%h #{basedir}/#{image_name}].chomp!
     end    
   end
   
--- site_files/public/assets/images/make_thumbs.sh
+++ site_files/public/assets/images/make_thumbs.sh
@@ -6,6 +6,6 @@ for i in $(ls -1)
 do
   if [ -f $i ] && [ "$i" != "${0##*/}" ]
   then	
-    $MAGICK_HOME/utilities/convert $i -resize 192x142! thumbs/$i
+    convert $i -resize 192x142! thumbs/$i
   fi
 done
