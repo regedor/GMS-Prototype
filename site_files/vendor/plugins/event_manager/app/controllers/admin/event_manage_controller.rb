61426e51c039fbe65b7566377696a159ca707189
Zamith <zamith.28@gmail.com>
2011-09-28 20:46:33 +0100

    Beautifing excel file creation code in events

--- site_files/vendor/plugins/event_manager/app/controllers/admin/event_manage_controller.rb
+++ site_files/vendor/plugins/event_manager/app/controllers/admin/event_manage_controller.rb
@@ -2,26 +2,22 @@ class Admin::EventManageController < Admin::BaseController
   filter_access_to :all, :require => any_as_privilege
 
   def index
-    @event = Event.find(params[:event_id])
-    @users = @event.users.paginate(:page => params[:page], :per_page => 2)
+    setup_variables
   end
     
   def download
-    @event = Event.find(params[:event_id])
+    setup_variables
     all_users = @event.users
-    @users = all_users.paginate(:page => params[:page], :per_page => 2)
+    path = "#{RAILS_ROOT}/public/excel-file.xls"
+    @event.create_excel_file path
+    send_file path, :type=>"application/vnd.ms-excel"
+  end    
     
-    Spreadsheet.client_encoding = 'UTF-8'
-    book = Spreadsheet::Workbook.new
-    sheet = book.create_worksheet :name => 'Subscribed Users'
-    sheet.row(0).concat ["Name", "E-Mail"]
-    all_users.each_index do |index|
-      user = all_users[index]
-      sheet.row(index+1).concat [user.name,user.email]
-    end  
-    book.write "#{RAILS_ROOT}/public/excel-file.xls"
-    send_file "#{RAILS_ROOT}/public/excel-file.xls", :type=>"application/vnd.ms-excel"
-    #redirect_to :action => 'index', :controller => 'admin/event_manage'
+  private
+  
+  def setup_variables
+    @event = Event.find(params[:event_id])
+    @users = @event.users.paginate(:page => params[:page], :per_page => 15)
   end    
     
 end
