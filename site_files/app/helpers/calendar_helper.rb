8b2dc615ca9a328e442816866d3d153613cabbe4
Pedro Pereira <pedro.mdp@gmail.com>
2011-11-29 14:27:06 +0000

    added calendar localization strings; added calendar arrows; added calendar title

--- site_files/app/helpers/calendar_helper.rb
+++ site_files/app/helpers/calendar_helper.rb
@@ -1,6 +1,14 @@
 module CalendarHelper
   def month_link(month_date)
-    link_to(I18n.localize(month_date, :format => "%B"), {:month => month_date.month, :year => month_date.year})
+    link_to(I18n.localize(month_date, :format => "%B"), { :month => month_date.month, :year => month_date.year })
+  end
+
+  def prev_month_link(month_date)
+    link_to("◄", { :month => month_date.month, :year => month_date.year })
+  end
+
+  def next_month_link(month_date)
+    link_to("►", { :month => month_date.month, :year => month_date.year })
   end
 
   # custom options for this calendar
@@ -10,8 +18,8 @@ module CalendarHelper
       :month => month,
       :event_strips => event_strips,
       :month_name_text => I18n.localize(shown_month, :format => "%B %Y"),
-      :previous_month_text => "<< " + month_link(shown_month.prev_month),
-      :next_month_text => month_link(shown_month.next_month) + " >>"
+      :previous_month_text => prev_month_link(shown_month.prev_month),
+      :next_month_text => next_month_link(shown_month.next_month)
     }
   end
 
