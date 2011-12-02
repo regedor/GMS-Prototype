2029d76dc8ed74b7354d3e9a06b3736fca730692
Zamith <zamith.28@gmail.com>
2011-12-02 00:16:09 +0000

    Making gallery available for public, without insertion form

--- site_files/app/helpers/navigation_helper.rb
+++ site_files/app/helpers/navigation_helper.rb
@@ -7,7 +7,8 @@ module NavigationHelper
       pages=""
       Page.navigation_pages.viewable_only(current_user).each do |page|
         pages += tag("li",nil,true)+link_to(page.title, page_path(page))+tag("/li",nil,true) 
-      end  
+      end 
+      pages +=  tag("li",nil,true)+link_to("Gallery", gallery_index_path)+tag("/li",nil,true) #gallery hack
       pages
     end
     html += tag("/ul",nil,true)
