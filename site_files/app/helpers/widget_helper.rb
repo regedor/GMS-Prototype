74b23f1dc1c626faba1ff35ecc2d48e79e837adb
Pedro Pereira <pedro.mdp@gmail.com>
2012-01-18 12:21:46 +0000

    lcb

--- site_files/app/helpers/widget_helper.rb
+++ site_files/app/helpers/widget_helper.rb
@@ -6,25 +6,25 @@ module WidgetHelper
   end
   
   # Show tags as a 2 level menu of limit elements (can be acompanied by the tag_header)
-  def tag_menu_widget(tags,limit=3)
-    tags_for_cloud = Tag.tags_for_menu(tags,limit) 
+  def tag_menu_widget(tags, limit = 10)
+    tags_for_cloud = Tag.tags_for_menu(tags, limit) 
     render :partial => 'widgets/tag_menu', 
-           :locals => {:tags_for_cloud => tags_for_cloud, :tags => tags}
+           :locals  => { :tags_for_cloud => tags_for_cloud, :tags => tags }
   end
   
   # Show tags as a cloud (should be acompanied by the tag_header)
   def tag_cloud_widget(tags)
     tags_for_cloud = Tag.tags_for_cloud(tags)
     render :partial => 'widgets/tag_cloud', 
-           :locals => {:tags_for_cloud => tags_for_cloud, :tags => tags}
+           :locals  => { :tags_for_cloud => tags_for_cloud, :tags => tags }
   end
   
   # List of tags being used takes optional parameter to set cloud or menu
-  def tag_header(tags,cloud=true)
+  def tag_header(tags, cloud = true)
     if tags
       tags = tags[0..1] unless cloud
-      render :partial => 'widgets/tag_cloud_header', 
-             :locals => {:tags => tags}
+      render :partial => 'widgets/tag_cloud_header',
+             :locals  => { :tags => tags }
     end
   end
 end
