e6b5db2ae70e5d447165dbc21c27d8a57febca24
Miguel Regedor <miguelregedor@gmail.com>
2012-01-24 18:27:54 +0000

    debug_bar created

--- site_files/app/controllers/sys_admin_controller.rb
+++ site_files/app/controllers/sys_admin_controller.rb
@@ -4,5 +4,17 @@ class SysAdminController < ApplicationController
     log = %x[tail -n 200 log/production.log].gsub("\n",'<br />')
     render :text => log
   end
+  
+  def update_current_user
+    if configatron.debug_bar == "true" && User.find(parmas[:user][:id]).update_attributes(parmas[:user])
+    user = current_user
+    user.role_id = params[:user][:role_id]
+    if user.save
+      flash[:notice] = "Done"
+    else
+      flash[:error] = "error" 
+    end
+    redirect_to admin_root_path
+  end
 
 end
