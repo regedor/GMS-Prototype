dfa35a486ca67f242aec7aaa317a5246a2c3c925
Miguel Regedor <miguelregedor@gmail.com>
2012-01-25 16:43:05 +0000

    sys debug system contoller

--- site_files/app/controllers/sys_admin_controller.rb
+++ site_files/app/controllers/sys_admin_controller.rb
@@ -6,14 +6,9 @@ class SysAdminController < ApplicationController
   end
   
   def update_current_user
-    if configatron.debug_bar == "true" && User.find(parmas[:user][:id]).update_attributes(parmas[:user])
     user = current_user
     user.role_id = params[:user][:role_id]
-    if user.save
-      flash[:notice] = "Done"
-    else
-      flash[:error] = "error" 
-    end
+    configatron.debug_bar && user.save ? flash[:notice] = "Done" : flash[:error] = "Error happened" 
     redirect_to admin_root_path
   end
 
