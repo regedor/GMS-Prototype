41af489dd5a55a308e8520c99c69c764aa4237c1
Miguel Regedor <miguelregedor@gmail.com>
2012-01-27 02:27:26 +0000

    correction in db seeds

--- site_files/db/seeds.rb
+++ site_files/db/seeds.rb
@@ -1,34 +1,23 @@
-puts "Creating settings..."
+unless Setting.find_by_identifier "frontend_navigation"
+  puts "Creating settings..."
   Setting.create :identifier => "frontend_navigation", :field_type => "string", :value => "<li><a href=\"/\">Notícias</a></li>__SelectedPages__"
+end
 
-puts "Creating standard roles..."
+if Role.count == 0
+  puts "Creating standard roles..."
   symbols = []
   File.open( "config/authorization_rules.rb" ) do |f| 
     f.grep( /role :([a-z_0-9]*) do/ ) { |line| symbols << $1 }
   end 
   symbols.reverse[1..-1].each { |role| Role.create :label => role }
+end
 
-puts "Creating standard groups..."
-  default_group = Group.create :name => "Default",           :mailable => false
+if Group.count == 0
+  puts "Creating standard groups..."
+  Group.create :name => "Group Buddies", :mailable => false
+end
 
-puts "Creating admins..."
-  root = User.new
-  root.email                 = 'contact@groupbuddies.com'
-  root.password              = 'password'
-  root.password_confirmation = 'password'
-  root.name                  = 'Root'
-  root.language              = 'en'
-  root.country               = 'PT'
-  root.phone                 = "123456789"
-  root.gender                =  true
-  root.role_id               =  Role.id_for :root
-  root.save
-  root.activate!
-  default_group.direct_users << root
-
-puts "Saving Groups..."
-  default_group.save
-
-puts "Creating Category"
-  GlobalCategory.create :name => "Default", :slug => "default" if GlobalCategory.all.empty?
-
+if GlobalCategory.all.empty?
+  puts "Creating Category"
+  GlobalCategory.create :name => "Default", :slug => "default" 
+end
