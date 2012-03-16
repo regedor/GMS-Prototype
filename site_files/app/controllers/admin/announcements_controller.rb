314d4527d6833c3b8c10c77ee74f31145500be52
Zamith <zamith.28@gmail.com>
2012-03-16 18:38:48 +0000

    Announcement cache

--- site_files/app/controllers/admin/announcements_controller.rb
+++ site_files/app/controllers/admin/announcements_controller.rb
@@ -1,6 +1,6 @@
 class Admin::AnnouncementsController < Admin::BaseController
   filter_access_to :all, :require => any_as_privilege
-
+  cache_sweeper :announcement_sweeper, :only => [:update,:create,:destroy]
   before_filter :date_localization, :only => [ :create, :update ]
   before_filter :validate_data, :only => [ :create, :update ]
 
