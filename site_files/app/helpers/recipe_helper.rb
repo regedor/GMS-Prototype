f2a8e935bc8445865108d221a280bfec903440a3
Zamith <zamith.28@gmail.com>
2012-03-23 17:30:05 +0000

    Culinary frontend

--- site_files/app/helpers/recipe_helper.rb
+++ site_files/app/helpers/recipe_helper.rb
@@ -1,26 +1,20 @@
 module RecipeHelper
 
+
   # Acceptable types are :sobremesa, :prato_principal, :entrada
   def recipe_for_today(type = :entrada)
     category = type.titleize
-    category_id = cache.fetch("recipe-category-for-#{type}") do
-      RecipeCategory.find_by_name(category, :select => :id)
-    end
+    category_id = Rails.cache.fetch("recipe-category-for-#{type}"){
+      RecipeCategory.find_by_name(category, :select => :id).id
+    }
+    
+    p category_id
     recipe = Recipe.find(:last, 
         :conditions=>["DATE(publication_date) <= ? and recipe_category_id = ?",Date.today, category_id], 
         :include => :image)
 
     if recipe
-      raw(
-        content_tag :span, :class => "recipe-title" do
-              recipe.name
-        end
-      )+     
-      raw(
-        content_tag :span, :class => "recipe-image" do
-              image_tag(recipe.image.multi_purpose_image.url(:thumb))
-        end
-      )
+      render 'recipes/todays_recipe', :recipe => recipe
     else
       "Não há #{category} para hoje... :("
     end
