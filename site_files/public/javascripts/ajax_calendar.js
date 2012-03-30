b41b1917f597e07981ffe86efcdf9415b6c5a286
Zamith <zamith.28@gmail.com>
2012-03-30 10:45:07 +0100

    Beautifing code

--- site_files/public/javascripts/ajax_calendar.js
+++ site_files/public/javascripts/ajax_calendar.js
@@ -1,9 +1,11 @@
 $(document).ready(function($){
   $("th.ec-month-nav").live('click',function(event){
     var query_params = $(this).find("a").attr("href").split("?")[1];
+
     $('div#event-calendar').load("/calendar?"+query_params,function(){
       $('div.events').parent().addClass("has-events");
     });
+
     event.preventDefault();
   });
 });
