1c13b12d1bcafff5bea75cad7dfc46eae507fcea
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-09 14:05:28 +0100

    fixed menu animation

--- site_files/public/javascripts/gb.js
+++ site_files/public/javascripts/gb.js
@@ -3,23 +3,25 @@ jQuery.noConflict();
 jQuery(document).ready(function($) {
 
   var header_logo = {
-   change_at: $("#who-we-are").offset().top-120,
-   from: "logo",
-   to: "lettering",
-   current: "logo",
-   
-   init : function(){
-     $(window).scroll(function(){
-       var current_pos = $(window).scrollTop();
-       console.log(header_logo.change_at);
-       console.log(current_pos)
-       if(current_pos >= header_logo.change_at && header_logo.current === "logo")
-       {
-          $(".logo").stop().animate({opacity: 0},1000,function(){
-            $(this).css({'background-image': "url('/images/logo_lettering.png')", "background-repeat": "no-repeat", "width": "190px"})
-                   .animate({opacity: 1, height: '33px'},{queue:false,duration:1000});
+    change_at: $("#who-we-are").offset().top-700,
+    from: "logo",
+    to: "lettering",
+    current: "logo",
+
+    init : function(){
+      $(window).scroll(function(){
+        var current_pos = $(window).scrollTop();
+        console.log(header_logo.change_at);
+        console.log(current_pos)
+        if(current_pos >= header_logo.change_at && header_logo.current === "logo")
+        {
+          $(".logo").stop().animate({"opacity": "0"}, 300, function() {
+            $(this).css({"background-image": "url('/images/logo_lettering.png')", "width": "190px"})
+              .animate({"opacity": "1"}, {queue: false, duration: 500})
+              .animate({"padding-bottom": "13px", "height": "33px"}, {duration: 500});
+            $(this).css({"background-position-y": "12px"});
           });
-          $("#slogan").fadeOut(600);
+          $("#slogan").fadeOut(300);
 
           header_logo.from = "lettering";
           header_logo.to   = "logo";
@@ -27,12 +29,12 @@ jQuery(document).ready(function($) {
         }
         else if(current_pos < header_logo.change_at && header_logo.current === "lettering")
         {
-          $(".logo").stop().animate({opacity: 0},400,function(){
-            $(this).css({'background-image': "url('/logo.png')", "background-repeat": "no-repeat", "width": "130px"})
-                   .animate({height: '92px'},{duration:1000})
-                   .animate({opacity: 1},{duration:1000});
+          $(".logo").stop().animate({"opacity": "0"}, 300, function() {
+            $(this).css({"background-image": "url('/logo.png')", "background-position-y": "0", "padding-bottom": "0", "width": "130px"})
+              .animate({"height": "92px"}, {duration: 300})
+              .animate({"opacity": "1"}, {duration: 300});
           });
-          $("#slogan").fadeIn(600);
+          $("#slogan").delay(500).fadeIn(300);
 
           header_logo.from = "logo";
           header_logo.to   = "lettering";
@@ -41,7 +43,6 @@ jQuery(document).ready(function($) {
       });
     }
   }
-  
+
   header_logo.init();
-  
 });
